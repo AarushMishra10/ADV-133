c4295d46780ff46a64117b4cf515a16472aacd00
AarushMishra10 <77782297+AarushMishra10@users.noreply.github.com>
2022-06-01 12:17:38 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,54 @@
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Object";
+    document.getElementById("answer_fan").innerHTML="There is only 1 big object image from which cocossd model has detected 1 object.";
+
+}
+
+
+function modelLoaded() { 
+    console.log("Model Loaded!") ;
+    status = true; 
+    objectDector.detect(img, gotResult); 
+}
+
+img="";
+status="";
+object=[];
+
+
+function preload(){
+img=loadImage("fan.jpg");
+
+}
+
+
+function draw(){
+    image(img,0,0,640,420);
+    if(status!=""){
+        for(i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            fill("red");
+            percennt=floor(object[i].confidence*100);
+            text(object[i].label+"  "+percennt+"%",object[i].x+15,object[i].y+15);
+            noFill();
+            stroke("red");
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+    
+        }
+    
+
+}
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+
+        
+    }
+    console.log(results);
+    object=results;
+}
